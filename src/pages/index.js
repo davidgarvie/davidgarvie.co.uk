7c52e122b6831bbb83d06b985aa5fa19f1ab5a31
David Garvie <garvie88@gmail.com>
2020-05-13 01:36:48 +0100

    feat(index-page): add index page

---
+++ src/pages/index.js
@@ -0,0 +1,35 @@
+import React from "react"
+import { graphql, Link } from 'gatsby'
+import { format } from 'date-fns'
+
+export default function Home(props) {
+  const data = props.data.allContentfulPost.nodes;
+  return (
+    <main>
+      <h1>David's Blog</h1>
+      <ul className="list">
+        {data.map(post => (
+          <li key={post.slug}>
+            <h2>
+              <Link to={`/posts/${post.slug}`}>{post.title}</Link>
+            </h2>
+            <span>{format(new Date(post.createdAt), 'do MMM')}</span>
+          </li>
+        ))}
+      </ul>
+    </main>
+  )
+}
+
+
+export const query = graphql`
+query HomePageQuery {
+  allContentfulPost {
+    nodes {
+      createdAt
+      slug
+      title
+    }
+  }
+}
+`
