19da45146425c96833450f0c9f8d45c18a38f243
David Garvie <garvie88@gmail.com>
2020-05-13 01:36:05 +0100

    feat(post-template): add post template

---
+++ src/templates/post.js
@@ -0,0 +1,48 @@
+import React from "react"
+import { graphql } from 'gatsby'
+import { formatDistance } from 'date-fns'
+import styles from "./post.module.css"
+
+function postTransformer(data) {
+  const { contentfulPost : { content, title, updatedAt } } = data;
+  const { html, timeToRead } = content.childMarkdownRemark;
+
+  return {
+    date: formatDistance(new Date(updatedAt), new Date()),
+    html,
+    timeToRead,
+    title,
+  }
+}
+
+export default function BlogPost({ data }) {
+  const { date, html, timeToRead, title } = postTransformer(data);
+  return (
+    <main className={styles.container}>
+      <h1>{title}</h1>
+      <p>Last updated {date} ago · <span>{timeToRead}</span> minutes to read.</p>
+      <article 
+        dangerouslySetInnerHTML={{
+          __html: html
+        }}
+      />
+    </main>
+  )
+}
+
+
+export const query = graphql`
+  query GetPost($slug: String) {
+    contentfulPost(slug: {eq: $slug }) {
+      content {
+        childMarkdownRemark {
+          html
+          timeToRead
+        }
+      }
+      slug
+      title
+      updatedAt
+    }
+  }
+`
