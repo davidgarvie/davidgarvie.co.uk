bcdd4e68e2d452ecf4b5f378a46a0d1c1dc1707d
David Garvie <garvie88@gmail.com>
2020-05-13 14:29:13 +0100

    :sparkles: (contact) add contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,31 @@
+import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope,} from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin,} from '@fortawesome/free-brands-svg-icons'
+import styles from './contact.module.css'
+
+export default function Contact(props) {
+
+  return (
+    <main className={styles.contact}>
+      <h1>Say hi.</h1>
+      <p>Get in touch by completing the form below:</p>
+      <form className={styles.form}>
+        <label htmlFor="name">Name:</label>
+        <input type="text" name="name" required placeholder="Name"></input>
+        <label htmlFor="email">Email:</label>
+        <input type="email" name="email" required placeholder="Email"></input>
+        <label htmlFor="phone">Phone Number:</label>
+        <input type="phone" name="phone" placeholder="Telephone number"></input>
+        <label htmlFor="enquiry">Enquiry:</label>
+        <textarea name="enquiry" placeholder="Your enquiry"></textarea>
+        <button type="submit">Submit</button>
+      </form>
+      <ul className={styles.links}>
+        <li classNamae={styles.email}><a href="mailto:garvie88@gmail.com"><FontAwesomeIcon icon={faEnvelope} alt="Email" /></a></li>
+        <li className={styles.github}><a href="https://github.com/davidgarvie"><FontAwesomeIcon icon={faGithub} alt="Github"/></a></li>
+        <li className={styles.linkedin}><a href="https://uk.linkedin.com/in/david-garvie"><FontAwesomeIcon icon={faLinkedin} alt="Linkedin"/></a></li>
+      </ul>
+    </main>
+  )
+}
