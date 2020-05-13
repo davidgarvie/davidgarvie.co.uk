3efbe8b3ef3fba54eebb16eabb2b78ac257d8fde
David Garvie <garvie88@gmail.com>
2020-05-13 12:26:24 +0100

    :sparkles: (index-page) get index page content from CMS
    Add a graphql query to get index page content from GraphQL. Also use gatsby image to optimize image loading on index page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,15 @@
 import React from "react"
 import { graphql, Link } from 'gatsby'
-import { format } from 'date-fns'
+import Img from "gatsby-image"
 import styles from './index.module.css'
 
 export default function Home(props) {
+  const { backgroundImage, content, title } = props.data.allContentfulHomePage.nodes[0];
   return (
     <main className={styles.container}>
       <div className={styles.content}>
-        <h1>David Garvie</h1>
-        <p>I write code that runs on the world wide web.</p>
+        <h1>{title}</h1>
+        <p>{content}</p>
         <nav className={styles.nav}>
           <ul>
             {/* <li>Portfolio</li>
@@ -19,7 +20,30 @@ export default function Home(props) {
           </ul>
         </nav>
       </div>
-      <img src="https://images.ctfassets.net/ikj4hzldd7jr/16eFKLLHxrdhNX5vbCjTcQ/651f763d3da659e72f85eff95c4a42cf/code-coding-computer-data-574070.jpg"/>
+      <Img fluid={backgroundImage.fluid} style={{
+        left: 0,
+        minHeight: '100vh',
+        position: 'absolute',
+        top: 0,
+        width: '100%',
+        zIndex: -1
+      }} />
     </main>
   )
 }
+
+export const query = graphql`
+  query GetHomePageContent {
+    allContentfulHomePage {
+      nodes {
+        content
+        title
+        backgroundImage {
+          fluid {
+            ...GatsbyContentfulFluid_withWebp
+          }
+        }
+      }
+    }
+  }
+`
