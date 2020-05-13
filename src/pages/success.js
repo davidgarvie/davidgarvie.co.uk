e1aa9fe47aaec4e68584c402d789fc4d5851fdbe
David Garvie <garvie88@gmail.com>
2020-05-13 16:58:20 +0100

    :recycle: (layout) pdate success page to use layout component

--- src/pages/success.js
+++ src/pages/success.js
@@ -1,11 +1,11 @@
 import React from "react"
-import styles from './success.module.css'
+import Layout from '../shared/layout';
 
 export default function Success(props) {
   return (
-    <main className={styles.success}>
-      <h1>Happy days</h1>
-      <p>Thanks for getting in touch. Grab a cuppa, relax and I'll be sure to respond as soon as possible.</p>
-    </main>
+    <Layout>
+        <h1>Happy days</h1>
+        <p>Thanks for getting in touch. Grab a cuppa, relax and I'll be sure to respond as soon as possible.</p>
+    </Layout>
   )
 }
